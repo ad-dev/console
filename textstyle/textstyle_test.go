180cbfab7daecfb1451c946a3bd2ef7875cfe281
ad <xad.dev.mail@gmail.com>
2025-05-17 18:24:11 +0000

    RGB test

--- textstyle/textstyle_test.go
+++ textstyle/textstyle_test.go
@@ -13,3 +13,13 @@ func TestStyle(t *testing.T) {
 		assert.Equal(t, fmt.Sprintf("\033[%dmtest\033[0m", i), FormatString("test", byte(i)))
 	}
 }
+
+func TestRGBColorStyle(t *testing.T) {
+	formattedString := FormatString("test", 38, 2, 255, 240, 200, 48, 2, 120, 110, 100)
+	assert.Equal(
+		t,
+		"\033[38;2;255;240;200;48;2;120;110;100mtest\033[0m",
+		formattedString,
+	)
+	t.Log(formattedString)
+}
